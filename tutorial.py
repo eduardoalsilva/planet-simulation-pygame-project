75155fa499624f1fc1e8c5a91bb561c6c9d4c7fb
Eduardo Silva <eduardoalsilva45@gmail.com>
2023-06-22 17:01:07 -0300

    Atualizando o arquivo tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -7,21 +7,62 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BLUE = (100, 149, 237)
+RED = (188, 39, 50)
+
+class Planet:
+    AU = 149.6e6 * 1000
+    G = 6.67428e-11
+    SCALE = 250 / AU
+    TIMESTEP = 3600 * 24
+
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+
+        self.x_vel = 0
+        self.y_vel = 0
+
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
+     
 
 
 def main():
     run = True
     clock = pygame.time.Clock()
+
+    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
+    sun.sun = True
+
+    earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+
+    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+
+    planets = [sun, earth, mars]
     
     while run:
         clock.tick(60)
-        WIN.fill(WHITE)
+        #WIN.fill(WHITE)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-    
+        for planet in planets:
+            planet.draw(WIN)
+
+        pygame.display.update()
+
     pygame.quit()
 
 main()
